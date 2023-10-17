073335079998c29c2f4bf030c88d5b2b5f7c81f0
jannis2301 <jannis2301@users.noreply.github.com>
2023-10-17 10:38:39 +0000

    deploy: 3bc983cd43c2d8045fb660bf95750cbd6dfa7a72

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import { RotatingPlanet } from './RotatingPlanet'
-
-const bootstrapper = () => {
-  new RotatingPlanet(
-    document.querySelector('.rotating-planet-container') as HTMLElement
-  )
-}
-
-window.addEventListener('load', bootstrapper)
